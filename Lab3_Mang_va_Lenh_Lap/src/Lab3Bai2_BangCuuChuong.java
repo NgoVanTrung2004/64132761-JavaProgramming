6e8b7a089eb714bb7e4c8c78d021e1b07eeda986
NgoVanTrung2004 <trung.nv.64cntt@ntu.edu.vn>
2025-02-23 10:08:38 +0700

    Lab3Bai2_BangCuuChuong

---
+++ Lab3_Mang_va_Lenh_Lap/src/Lab3Bai2_BangCuuChuong.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Lab3Bai2_BangCuuChuong {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=9; i++) {
+			System.out.println("Bảng nhân " + i + ":");
+		for(int j=1; j<=10; j++) {
+			System.out.printf("%d x %d = %d%n", i, j, i*j);
+		}
+		System.out.println();
+		}
+	}
+
+}
