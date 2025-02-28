ebb0ef8453456498acd68405609602c2520e7a05
NgoVanTrung2004 <trung.nv.64cntt@ntu.edu.vn>
2025-02-28 20:27:39 +0700

    Create Lab4Bai2_2SanPham.java

---
+++ Lab4_Lop_va_DoiTuong/src/Lab4Bai2_2SanPham.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class SP {
+    String ten;
+    double gia;
+    double giamGia;
+
+    void nhap() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập tên sản phẩm: ");
+        ten = scanner.nextLine();
+        System.out.print("Nhập giá sản phẩm: ");
+        gia = scanner.nextDouble();
+        System.out.print("Nhập giảm giá: ");
+        giamGia = scanner.nextDouble();
+    }
+
+    void xuat() {
+        double thueNhapKhau = gia * 0.1;
+        System.out.println("Tên sản phẩm: " + ten);
+        System.out.println("Đơn giá: " + gia);
+        System.out.println("Giảm giá: " + giamGia);
+        System.out.println("Thuế nhập khẩu: " + thueNhapKhau);
+    }
+}
+
+public class Lab4Bai2_2SanPham {
+    public static void main(String[] args) {
+        SanPham sp1 = new SanPham();
+        SanPham sp2 = new SanPham();
+        
+        System.out.println("Nhập thông tin sản phẩm thứ nhất:");
+        sp1.nhap();
+        
+        System.out.println("Nhập thông tin sản phẩm thứ hai:");
+        sp2.nhap();
+        
+        System.out.println("Thông tin sản phẩm thứ nhất:");
+        sp1.xuat();
+        
+        System.out.println("Thông tin sản phẩm thứ hai:");
+        sp2.xuat();
+    }
+}
